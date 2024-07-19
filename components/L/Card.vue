0a622d3c063f825d1bb92fcf9ce96235fd6beb36
kstraszewski <koonradstraszewski@gmail.com>
2024-07-19 22:41:20 +0200

    add Divider and Card

--- components/L/Card.vue
+++ components/L/Card.vue
@@ -1,8 +1,38 @@
 <template>
-  <div>
-    Karteczka z zawartością i tytułem
-    <slot/>
+  <div class="border">
+    <!-- Slot title -->
+    <template v-if="slots.title">
+      <div class="headline-1">
+        <slot name="title"/>
+      </div>
+      <LDivider/>
+    </template>
+
+    <!-- Slot content -->
+    <div class="content">
+      <slot name="content"/>
+    </div>
+
+    <!-- Slot actions -->
+    <template v-if="slots.actions">
+      <LDivider/>
+      <div class="actions">
+        <slot name="actions"/>
+      </div>
+    </template>
   </div>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import type {VNode} from "vue";
+
+const slots = defineSlots<{
+  actions?: ((props: { }) => VNode)
+  content: ((props: { }) => VNode)
+  title?: ((props: { }) => VNode)
+}>();
+</script>
+
+<style scoped lang="scss">
+
+</style>
---
+++ components/L/Divider.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="flex items-center justify-center w-full relative my-4">
+    <template v-if="props.label">
+      <span class="bg-white px-4 z-10">{{ props.label }}</span>
+    </template>
+    <span class="absolute left-0 top-1/2 w-full border-t border-gray-300"></span>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { defineProps } from 'vue';
+
+type DividerProps = {
+  label?: string;
+};
+
+const props = defineProps<DividerProps>();
+</script>
