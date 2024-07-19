6a79ce0a42622bab6bda46c442e3ddfd448b026f
kstraszewski <koonradstraszewski@gmail.com>
2024-07-19 22:16:22 +0200

    Create formatNumber.ts

---
+++ utils/formatNumber.ts
@@ -0,0 +1,7 @@
+export const formatNumber = (number: number) => {
+  const { format: formatNumber } = Intl.NumberFormat('pl-PL', {
+    notation: 'compact',
+    maximumFractionDigits: 1
+  })
+  return formatNumber(number)
+}
