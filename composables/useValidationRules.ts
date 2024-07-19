8d5fdc1a43a2ceced5617e788332a5461fc41104
kstraszewski <koonradstraszewski@gmail.com>
2024-07-19 22:06:47 +0200

    Create useValidationRules.ts

---
+++ composables/useValidationRules.ts
@@ -0,0 +1,11 @@
+type Rule = {
+  name: string;
+  message: string;
+  validate: (value: string) => boolean;
+};
+
+export default function useValidationRules() {
+  return {
+    rules: [] as Rule[]
+  }
+}
