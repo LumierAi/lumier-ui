c9324d0639679732da465c33e23b4f0b4f40212e
kstraszewski <koonradstraszewski@gmail.com>
2024-11-19 19:32:32 +0100

    change to robot slab

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -3,7 +3,7 @@ import {type Config} from 'tailwindcss';
 export default <Partial<Config>>{
   theme: {
     fontFamily: {
-      sans: ["Nunito Sans", "Patua One"],
+      sans: ["Nunito Sans", "Roboto Slab"],
     },
   },
 };
