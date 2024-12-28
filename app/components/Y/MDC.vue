564da3041223a9275c7c350b8d4372af0951c77b
grgol <gr.golus@gmail.com>
2024-12-28 20:48:54 +0100

    feat: MDC, useMarkdown

---
+++ app/components/Y/MDC.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+const props = defineProps<{
+  content: string
+}>()
+
+const parsedContent = ref<string>(useMarkdown(props.content))
+
+watch(() => props.content, (newContent) => {
+  console.log('props.content', props.content)
+  parsedContent.value = useMarkdown(newContent)
+})
+</script>
+
+<template>
+  <div v-html="parsedContent" />
+</template>
