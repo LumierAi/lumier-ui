855031a1f376a6e6546a412770dafc183921f2f9
grgol <gr.golus@gmail.com>
2024-12-30 01:23:57 +0100

    rev: remove playground
    chore: remove ingors

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -21,8 +21,6 @@ export default defineNuxtConfig({
     join(__dirname, './app/assets/typography/index.css'),
   ],
 
-  ignore: ['./.playground'],
-
   future: {
     compatibilityVersion: 4,
   },
