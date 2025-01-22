2ca4503731a5d99de3bc1a36089d5214c5788451
kstraszewski <koonradstraszewski@gmail.com>
2025-01-22 23:05:55 +0100

    Update button.vue

--- app/pages/button.vue
+++ app/pages/button.vue
@@ -6,34 +6,43 @@ async function handlePromise() {
 
 <template>
   <div>
-    <YBtn
-      label="Click me"
-      :loading="true"
-      :click="() => handlePromise()"
-    />
-    <YBtn
-      label="Prepend icon me"
-      prepend-icon="tabler:a-b-off"
-      :click="() => handlePromise()"
-    />
-    <YBtn
-      icon="tabler:a-b-off"
-      :click="() => handlePromise()"
-    />
-    <YBtn
-      text
-      label="Click me"
-      :click="() => handlePromise()"
-    />
-    <YBtn
-      label="Click me"
-      :click="() => handlePromise()"
-    />
-    <YBtn
-      label="Click me small"
-      size="small"
-      :click="() => handlePromise()"
-    />
-    <TestButton />
+    <div>
+      <YBtn
+        label="Click me"
+        :loading="true"
+        :click="() => handlePromise()"
+      />
+      <YBtn
+        label="Prepend icon me"
+        prepend-icon="tabler:a-b-off"
+        :click="() => handlePromise()"
+      />
+      <YBtn
+        icon="tabler:a-b-off"
+        :click="() => handlePromise()"
+      />
+      <YBtn
+        text
+        label="Click me"
+        :click="() => handlePromise()"
+      />
+      <YBtn
+        label="Click me"
+        :click="() => handlePromise()"
+      />
+      <YBtn
+        label="Click me small"
+        size="small"
+        :click="() => handlePromise()"
+      />
+      <TestButton />
+    </div>
+    <div>
+      <YBtn
+        label="Click me"
+        block
+        :click="() => handlePromise()"
+      />
+    </div>
   </div>
 </template>
