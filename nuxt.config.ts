0ef54a530d7d0b70b62dda24e254de54209cbf92
grgol <gr.golus@gmail.com>
2025-01-29 00:11:03 +0100

    Update nuxt.config.ts

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -108,6 +108,7 @@ export default defineNuxtConfig({
         'AccordionTab',
         'DataTable',
         'Column',
+        'Popover',
         'ConfirmDialog',
         'Paginator',
         'Drawer',
