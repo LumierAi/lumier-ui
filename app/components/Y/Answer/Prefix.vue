824ce003703b2338b55e94071002c595234e5c72
kstraszewski <koonradstraszewski@gmail.com>
2025-04-09 22:54:32 +0200

    Update Prefix.vue

--- app/components/Y/Answer/Prefix.vue
+++ app/components/Y/Answer/Prefix.vue
@@ -2,19 +2,30 @@
 interface Props {
   prefix: string
   color?: 'success' | 'danger' | `#${string}` | null
+  size?: 'small' | 'medium' | 'large'
 }
 
-defineProps<Props>()
+const props = withDefaults(defineProps<Props>(), {
+  size: 'medium'
+})
+
 </script>
 
 <template>
   <div
-    class="flex items-center justify-center size-8 rounded-[14px]"
-    :class="color?.startsWith('#') ? `bg-[${color}]` : color ? `bg-${color}` : 'bg-gray-100'"
+    class="flex items-center justify-center rounded-[14px]"
+    :style="color?.startsWith('#') ? { backgroundColor: color } : {}"
+    :class="[
+      size === 'small' ? 'size-6' : size === 'medium' ? 'size-8' : 'size-10',
+      color?.startsWith('#') ? '' : color ? `bg-${color}` : 'bg-gray-100'
+      ]"
   >
     <span
-      class="headline-6 flex items-center justify-center"
-      :class="color ? `text-white` : 'text-gray-900'"
+      class="flex items-center justify-center"
+      :class="[
+        color ? `text-white` : 'text-gray-900',
+        size === 'medium' ? 'headline-6' : size === 'small' ? 'body-1' : 'headline-5'
+        ]"
     >
       <Icon v-if="prefix.startsWith('tabler:')" :name="prefix" class="size-6" />
       <span v-else>
