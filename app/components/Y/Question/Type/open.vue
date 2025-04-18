9a5833eb96ea6cf12483ab365b6d33eb67917baf
grgol <gr.golus@gmail.com>
2025-04-18 22:03:26 +0200

    Refactor open.vue to wrap elements in a div and add margin to YTextarea

--- app/components/Y/Question/Type/open.vue
+++ app/components/Y/Question/Type/open.vue
@@ -9,13 +9,16 @@ const label = computed<string>(() => props.question.body.question)
 </script>
 
 <template>
-  <!-- eslint-disable vue/no-mutating-props -->
-  <label :for="question.id" class="leading-8 text-xl font-bold">{{ label }}</label>
-  <YTextarea
-    :id="question.id"
-    v-model="question.userAnswer"
-    :rows="4"
-    placeholder="Wpisz swoją odpowiedź..."
-  />
-  <!-- eslint-enable vue/no-mutating-props -->
+  <div>
+    <!-- eslint-disable vue/no-mutating-props -->
+    <label :for="question.id" class="leading-8 text-xl font-bold">{{ label }}</label>
+    <YTextarea
+      :id="question.id"
+      v-model="question.userAnswer"
+      :rows="4"
+      class="mt-6"
+      placeholder="Wpisz swoją odpowiedź..."
+    />
+    <!-- eslint-enable vue/no-mutating-props -->
+  </div>
 </template>
